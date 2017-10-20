6c8bbc3bfe3d73283aeade4708f3c13b037c36a6
gmusial112 <g.musial@straz.lodz.pl>
2017-10-20 11:41:45 +0200

    error

--- program2.c
+++ program2.c
@@ -7,18 +7,28 @@ int mnoz(int a, int b)
 int dodaj (int a, int b)
 {
 	return a+b;
+int odejmij (int a, int b)
+{
+	return b-a;
 }
 int main (void)
 {
 	int a = 2, b = 5; /*deklaracja wraz zinicjalizacją*/
-	char wybor = '+';
+	char wybor = '-';
 	/* if(wyrazenie) (blok jesli wyrazenie prawdziwe)
 	else (blok jesli wyrazenie fałszywe); */
-	if(wybor == '*'){ /* sprawdzenie czy rowne, to nie jest '=" */
+	if(wybor == '*')
+	{ /* sprawdzenie czy rowne, to nie jest '=" */
 		printf("wynik mnozenia: %i\n", mnoz(a,b));
-	}else{
+		}{
+		if(wybor == '+')
+		{
 		printf("Wynik dodawania: %i\n", dodaj(a,b));
-	}; /* ten srednik jest opcjonalny jesli wystepuje klamra*/
+		}else{
+		 printf("Wynik odejmowania: %i\n", odejmij(a,b));
+		}
+	} 
+}; /* ten srednik jest opcjonalny jesli wystepuje klamra*/
 
  	return 0;
 
