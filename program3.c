2be5a7f5818d86d4effb9498dac6f31d5b556dcb
gmusial112 <g.musial@straz.lodz.pl>
2017-10-20 13:53:11 +0200

    kalkulator poprawiony

--- program3.c
+++ program3.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-int mnoz(int a, int b){ printf("Wynik mnożenia: %d\n", a * b);return 0; }
-int dodaj (int a, int b){ printf ("Wynik dodawania: %d\n", a + b);return 0;}
-int odejmij (int a, int b){ printf ("Wynik odejmowania: %d\n", a - b);return 0;}
-int dziel (int a, int b){ printf ("Wynik dzielenia: %d\n", a / b);return 0;}
+int mnoz(int a, int b){ printf("Wynik mnożenia: %d\n", a * b);return a*b; }
+int dodaj (int a, int b){ printf ("Wynik dodawania: %d\n", a + b);return a+b;}
+int odejmij (int a, int b){ printf ("Wynik odejmowania: %d\n", a - b);return a-b;}
+int dziel (int a, int b){ printf ("Wynik dzielenia: %d\n", a / b);return a/b;}
 
 int main (void)
 {
