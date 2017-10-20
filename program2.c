4eed0cf030f8cf068c8a868ceb2abd751da386cb
gmusial112 <g.musial@straz.lodz.pl>
2017-10-20 11:52:43 +0200

    kolejna zmiana

--- program2.c
+++ program2.c
@@ -7,27 +7,20 @@ int mnoz(int a, int b)
 int dodaj (int a, int b)
 {
 	return a+b;
-int odejmij (int a, int b)
-{
-	return b-a;
 }
+
 int main (void)
 {
 	int a = 2, b = 5; /*deklaracja wraz zinicjalizacją*/
-	char wybor = '-';
+	char wybor;
 	/* if(wyrazenie) (blok jesli wyrazenie prawdziwe)
 	else (blok jesli wyrazenie fałszywe); */
-	if(wybor == '*')
-	{ /* sprawdzenie czy rowne, to nie jest '=" */
+	scanf("%c",&wybor); /* czytaj z consoli i zapisz wartosc pod wybor */
+	if(wybor == '*'){  /* sprawdzenie czy rowne, to nie jest '=" */
 		printf("wynik mnozenia: %i\n", mnoz(a,b));
-		}{
-		if(wybor == '+')
-		{
-		printf("Wynik dodawania: %i\n", dodaj(a,b));
-		}else{
-		 printf("Wynik odejmowania: %i\n", odejmij(a,b));
-		}
-	} 
+	}else{
+
+		printf("Wynik dodawania: %i\n", dodaj(a,b)); 
 }; /* ten srednik jest opcjonalny jesli wystepuje klamra*/
 
  	return 0;
