08508db62e681368076bc400ff54f2c5ce1e72e1
gmusial112 <g.musial@straz.lodz.pl>
2017-10-20 09:37:58 +0200

    implementacja pierwszych programow

---
+++ program.c
@@ -0,0 +1,19 @@
+#include <stdio.h> /*"biblioteka standardowa input-output */
+/* y = f(x) */
+/*     f() =>0 */
+int main () /* funkcja główna programu */
+/* int -> typ zwracanej wartości ( int wartosc calkowita) */
+{
+	puts("Pierwsza Zmiana"); /* puts - wyświetla ciąg znaków */
+
+	putchar('g');
+	putchar('\n');/*znak końca lini*/
+	putchar('\t'); /*tabulator*/
+	putchar('G');
+	putchar('\r'); /*powrót karetki kursor powraca na początek linii*/
+	putchar('\\'); /*wypisuje \ */
+	putchar('\n');
+	return 0;
+}/* klamry oznaczają blok programu zwykle ciało funkcji*/
+
+
