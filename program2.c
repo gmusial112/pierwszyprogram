2760aef2918240a523d766c21dcf79631f17110e
gmusial112 <g.musial@straz.lodz.pl>
2017-10-20 11:09:20 +0200

    Drugi program

---
+++ program2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int mnoz(int a, int b)
+{
+	return a*b;
+}
+int dodaj (int a, int b)
+{
+	return a+b;
+}
+int main (void)
+{
+	int a = 2, b = 5; /*deklaracja wraz zinicjalizacją*/
+	char wybor = '*';
+	/* if(wyrazenie) (blok jesli wyrazenie prawdziwe)
+	else (blok jesli wyrazenie fałszywe); */
+	if(wybor == '*'){ /* sprawdzenie czy rowne, to nie jest '=" */
+		printf("wynik mnozenia: %i\n", mnoz(a,b));
+	}else{
+		printf("Wynik dodawania: %i\n", dodaj(a,b));
+	}; /* ten srednik jest opcjonalny jesli wystepuje klamra*/
+
+ 	return 0;
+
+
+}
